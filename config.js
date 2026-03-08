ba90ac2146a61ba94012407b73fbe7e8362c3b10
Wmk234 <wilsonkiala00@gmail.com>
2026-03-08 14:32:13 +0100

    Change news API key to a new value
    Updated news API key to a new value.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Substitua pelas suas chaves reais
-const newsApiKey = "SUA_CHAVE_NOTICIAS";
+const newsApiKey = "d290476b6a4d47e281e3c7e83119d199";
 const stripePublicKey = "SUA_CHAVE_STRIPE_PUBLICA";
